2fa336e31bbf9d9ac3fb4fed7a054aad42060a1e
hollandcasino <hollandcasino@spritecloud.com>
2018-02-23 22:02:08 +0200

    Lesson 4 commit

--- src/test/java/LinkedinLoginPage.java
+++ src/test/java/LinkedinLoginPage.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LinkedinLoginPage {
 	WebDriver driver;
@@ -9,12 +12,16 @@ public class LinkedinLoginPage {
 		this.driver = driver;
 	}
 
+    @FindBy
 	private WebElement emailField;
+
+
 	private WebElement passwordField;
 	private WebElement signInButton;
 
 	private void initElements(){
 		emailField = driver.findElement(By.id("login-email"));
+		waitUntilElementIsClickable(emailField, 5);
 		passwordField = driver.findElement(By.id("login-password"));
 		signInButton = driver.findElement(By.id("login-submit"));
 	}
@@ -24,6 +31,16 @@ public class LinkedinLoginPage {
 		emailField.sendKeys(email);
 		passwordField.sendKeys(password);
 		signInButton.click();
+	}
+
+	public void waitUntilElementIsClickable (WebElement webElement){
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(webElement));
+	}
+
+	public void waitUntilElementIsClickable (WebElement webElement, int timeOutInSeconds){
+		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+		wait.until(ExpectedConditions.elementToBeClickable(webElement));
 	}
 
 
--- src/test/java/LinkedinSearchTest.java
+++ src/test/java/LinkedinSearchTest.java
@@ -1,8 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class LinkedinSearchTest {
 	WebDriver driver;
@@ -10,6 +17,7 @@ public class LinkedinSearchTest {
 	@BeforeMethod
 	public void beforeTest(){
 		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.linkedin.com/");
 	}
 
@@ -19,13 +27,40 @@ public class LinkedinSearchTest {
 	}
 
 	@Test
-	public void basicSearchTest() {
+	public void basicSearchTest() throws InterruptedException {
 		LinkedinLoginPage loginPage = new LinkedinLoginPage(driver);
-		loginPage.loginAs("iteatest@i.ua", "1q2w3e_4r");
-
-
+		loginPage.loginAs("iteatest@i.ua", "1q2w3e_4r5t");
 
 		//search
+		String searchTerm = "HR";
+
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys(searchTerm);
+		driver.findElement(By.xpath("//*[@type='search-icon']")).click();
+         //[contains(@class,'search-result__occluded-item')]
+
+		List<WebElement> results = driver.findElements(By.xpath("//li[contains(@class,'search-result__occluded-item')]"));
+		int currentResultsNumber = results.size();
+		Assert.assertEquals(results.size(), 10, "Number of results is wrong");
+
+
+		for (int i = 1; i < results.size(); i++) {
+			((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", results.get(i));
+
+			String cardTitle = driver.findElement(
+					By.xpath("//li[contains(@class,'search-result__occluded-item')]["+i+"]//span[contains(@class, 'actor-name')]")).getText();
+			System.out.println(cardTitle);
+			Assert.assertTrue(cardTitle.contains(searchTerm.toLowerCase()),
+					"Searchterm "+searchTerm+ "not found in cart number"+ Integer.toString(i));
+		}
+
+
+
+		//List<WebElement> cardTitles = driver.findElements(By.xpath("//li[contains(@class,'search-result__occluded-item')]//span[contains(@class, 'actor-name')]"));
+
+
+
+
+
 		//input[@placeholder='Search']
 		//*[@type='search-icon']
 		//div[contains(@class,'search-result--person')]
